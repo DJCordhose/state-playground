3ab00684f9dbeb932830e833b2d027ff552b87bd
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2021-05-21 14:44:27 +0200

    context example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {createContext, useContext} from 'react';
+
+const baseUrl = {
+  host: "localhost",
+  port: 8080
+}
+
+// https://reactjs.org/docs/hooks-reference.html#usecontext
+const UrlContext = createContext(baseUrl)
+
+
+function useUrl() {
+  const {host, port} = useContext(UrlContext);
+
+  return `${host}:${port}`;
+}
+
+function UrlDisplay() {
+  return (
+    <p>Url: {useUrl()}</p>
+  );
+}
+
+
+// function UrlDisplay() {
+//   const url = useContext(UrlContext);
+//   const {host, port} = url;
+//   return (
+//     <p>Url: {host}:{port}</p>
+//   );
+// }
+
+
+
 function App() {
   return (
     <div className="App">
+      <UrlContext.Provider value={baseUrl}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <UrlDisplay /> 
       </header>
+      </UrlContext.Provider>
     </div>
   );
 }
